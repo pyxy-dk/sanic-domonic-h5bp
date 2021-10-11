c6b05f97cb03d8437ca0480660166b7c74883830
Kristian Thy <k@pyxy.dk>
2021-10-11 12:46:50 +0200

    feat: linter script

---
+++ scripts/lint.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from __future__ import annotations
+
+from subprocess import run
+
+
+def run_with_separator(args):
+    """Print a horizontal rule to console and run a subprocess."""
+    print("\n", "=" * 80, "\n")
+    print(" ".join(args))
+    return run(args, check=False)
+
+
+def lint() -> int:
+    ISORT = run_with_separator(["isort", ".", "--atomic",])
+    BLACK = run_with_separator(["black", "--safe", "."])
+    return ISORT.returncode + BLACK.returncode
+
+
+if __name__ == "__main__":
+    raise SystemExit(lint())
