a80b786ff2dd7cf353a4b2b48e7c9506bf25aed3
Kristian Thy <k@pyxy.dk>
2021-10-11 12:47:20 +0200

    feat: initial Sanic app with static loader and 404

---
+++ src/sanic-domonic-h5bp/strings.py
@@ -0,0 +1,53 @@
+"""HTML5 Boilerplate strings."""
+
+NOT_FOUND_COMMENT = (
+    " IE needs 512+ bytes: "
+    "https://docs.microsoft.com/archive/blogs/ieinternals/friendly-http-error-pages "
+)
+NOT_FOUND_MESSAGE = "Sorry, but the page you were trying to view does not exist."
+NOT_FOUND_STYLE = """
+            * {
+                line-height: 1.2;
+                margin: 0;
+            }
+
+            html {
+                color: #888;
+                display: table;
+                font-family: sans-serif;
+                height: 100%;
+                text-align: center;
+                width: 100%;
+            }
+
+            body {
+                display: table-cell;
+                vertical-align: middle;
+                margin: 2em auto;
+            }
+
+            h1 {
+                color: #555;
+                font-size: 2em;
+                font-weight: 400;
+            }
+
+            p {
+                margin: 0 auto;
+                width: 280px;
+            }
+
+            @media only screen and (max-width: 280px) {
+
+            body,
+            p {
+                width: 95%;
+            }
+
+            h1 {
+                font-size: 1.5em;
+                margin: 0 0 0.3em;
+            }
+
+            }"""
+NOT_FOUND_TITLE = "Page Not Found"
